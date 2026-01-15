c0ed7d2f67d6c6a0ab2c1ad9da40b6ef9d7215ff
Mario Cortez <mrcortez566@gmail.com>
2026-01-15 11:48:12 -0300

    Inicial: Configuraciones y documento marco

---
+++ flightpredictor/src/main/java/com/fligthontime/flightpredictor/FlightpredictorApplication.java
@@ -0,0 +1,13 @@
+package com.fligthontime.flightpredictor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightpredictorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightpredictorApplication.class, args);
+	}
+
+}
---
+++ flightpredictor/src/test/java/com/fligthontime/flightpredictor/FlightpredictorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fligthontime.flightpredictor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightpredictorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
